13972e8cdcd69eacdab3d6a34a78e1abec411776
CxsGhost <1659101919@qq.com>
2020-04-24 15:12:27 +0800

    科学计算库的基础学习

---
+++ future.py
@@ -0,0 +1,16 @@
+a = [[] for _ in range(3)]
+print(a)
+
+
+class Uuu:
+    @staticmethod
+    def __nihoa():
+        print("rrrrrr")
+
+
+print(dir(Uuu))
+a = Uuu()
+a._Uuu__nihoa()
+print(a.__init__())
+
+
