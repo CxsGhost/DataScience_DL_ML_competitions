941447c88140b43cadb3bec480a4f74e20986642
CxsGhost <1659101919@qq.com>
2020-04-28 23:18:31 +0800

    科学计算库

--- Keras/MNIST数据集/Sequential类_network.py
+++ Keras/MNIST数据集/Sequential类_network.py
@@ -21,7 +21,8 @@ test_labels = to_categorical(test_labels)
 # 构建模型, Dense是全连接层，选择激第一个参数是节点数，依次是激活函数和输入数据大小（只第一层需要输入大小）
 network = models.Sequential()
 # 设置输入大小意味着是首个隐藏层,单条数据是784，第二个数据是批量维度，不给定具体则可以是任意
-network.add(layers.Dense(100, activation='relu', input_shape=(28 * 28, )))
+network.add(layers.Dense(500, activation='relu', input_shape=(28 * 28, )))
+network.add(layers.Dropout(0.3))  # 可以添加dropout层来防止过拟合
 network.add(layers.Dense(10, activation='softmax'))
 
 # 编译网络，选择优化器，损失函数（多元交叉熵），以及监控指标（这里只选了准确率）
@@ -30,7 +31,7 @@ network.compile(optimizer='rmsprop',
                 metrics=['accuracy'])
 
 # 训练网络, epoch遍历五遍数据，设置mini—batch大小， 并且每epoch会输出一次损失和准确率（基于训练数据）
-network.fit(x=train_images, y=train_labels, epochs=5, batch_size=128)
+network.fit(x=train_images, y=train_labels, epochs=15, batch_size=128)
 
 # 测试集数据,查看损失和准确率
 test_loss, test_accuracy = network.evaluate(test_images, test_labels)
