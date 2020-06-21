32ba878bc02cc843e739ec98c607cdc197e3b1b5
CxsGhost <1659101919@qq.com>
2020-06-22 00:28:25 +0800

    科学计算库

--- try1.py
+++ try1.py
@@ -1,101 +1,30 @@
-import numpy as np
+# import os
+#
+# _dir = 'C:/Users/16591/Desktop/寒假线上教学/大学体育'
+#
+# for i in range(1, 15):
+#     dir_ = os.path.join(_dir, '{}'.format(i))
+#     os.mkdir(dir_)
 
-import os
-
-data_dir = 'E:/py/科学计算与机器学习/Keras/自然语言及序列处理（RNN、LSTM、GRU、1DConv）' \
-           '/jean天气时间序列数据集/jena_climate_2009_2016.csv'
-fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
-
-f = open(fname)
-data = f.read()
-f.close()
-
-lines = data.split('\n')
-header = lines[0].split(',')
-lines = lines[1:]
-float_data = np.zeros((len(lines), len(header) - 1))
-
-for i, line in enumerate(lines):
-    values = [float(x) for x in line.split(',')[1:]]
-    float_data[i, :] = values
-print(header)
-print(len(lines))
-
-mean = float_data[:200000].mean(axis=0)
-float_data -= mean
-std = float_data[:200000].std(axis=0)
-float_data /= std
-
-def generator(data, lookback, delay, min_index, max_index,
-              shuffle=False, batch_size=128, step=6):
-    if max_index is None:
-        max_index = len(data) - delay - 1
-    i = min_index + lookback
-    while 1:
-        if shuffle:
-            rows = np.random.randint(
-                min_index + lookback, max_index, size=batch_size)
-        else:
-            if i + batch_size >= max_index:
-                i = min_index + lookback
-            rows = np.arange(i, min(i + batch_size, max_index))
-            i += len(rows)
-
-        samples = np.zeros((len(rows),
-                           lookback // step,
-                           data.shape[-1]))
-        targets = np.zeros((len(rows),))
-        for j, row in enumerate(rows):
-            indices = range(rows[j] - lookback, rows[j], step)
-            samples[j] = data[indices]
-            targets[j] = data[rows[j] + delay][1]
-        yield samples, targets
-
-
-lookback = 1440
-step = 6
-delay = 144
-batch_size = 128
-
-train_gen = generator(float_data,
-                      lookback=lookback,
-                      delay=delay,
-                      min_index=0,
-                      max_index=200000,
-                      shuffle=True,
-                      step=step,
-                      batch_size=batch_size)
-val_gen = generator(float_data,
-                    lookback=lookback,
-                    delay=delay,
-                    min_index=200001,
-                    max_index=300000,
-                    step=step,
-                    batch_size=batch_size)
-test_gen = generator(float_data,
-                     lookback=lookback,
-                     delay=delay,
-                     min_index=300001,
-                     max_index=None,
-                     step=step,
-                     batch_size=batch_size)
-
-# This is how many steps to draw from `val_gen`
-# in order to see the whole validation set:
-val_steps = (300000 - 200001 - lookback) // batch_size
-
-# This is how many steps to draw from `test_gen`
-# in order to see the whole test set:
-test_steps = (len(float_data) - 300001 - lookback) // batch_size
-
-
-from keras.models import Sequential
+from keras import Input
+from keras import models
 from keras import layers
-from keras.optimizers import RMSprop
+from keras import losses
 
-model = Sequential()
-model.add(layers.GRU(32,dropout = 0.2,recurrent_dropout = 0.2,input_shape = (None,float_data.shape[-1])))
-model.add(layers.Dense(1))
+_input = Input(shape=(None, ),
+               dtype='int32',
+               name='ceshi')
+s1 = layers.Dense(1, activation='sigmoid')(_input)
 
-model.compile(optimizer = RMSprop(),loss = 'mae')
-history = model.fit_generator(train_gen,steps_per_epoch = 500,epochs = 40,validation_data = val_gen,validation_steps = val_steps)
+i = [[1, 2, 3, 4], [1, 2, 3]]
+
+m = models.Model(inputs=_input, outputs=s1)
+m.compile(optimizer='rmsprop',
+          loss=losses.binary_crossentropy)
+
+print(m.summary())
+out = m.layers
+
+
+
+
